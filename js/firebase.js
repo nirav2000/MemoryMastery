6e2b03ef7f948d49ad3e0bdef30422592f53214b
nirav2000 <nirav2000+github@gmail.com>
2026-07-17 22:00:31 +0100

    Refactor firebaseConfig with additional properties
    Updated firebaseConfig to include storageBucket, messagingSenderId, and measurementId.

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,13 @@
 // Replace this public web configuration with values from Firebase Console.
-export const firebaseConfig={apiKey:'AIzaSyDBXGMlf7DWPAneD96kCwS9GzLMo2xb8dQ',authDomain:'memorymastery.firebaseapp.com',projectId:'memorymastery',appId:'1:439635305999:web:374ab9a9818b04c3dfe57a'};
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDBXGMlf7DWPAneD96kCwS9GzLMo2xb8dQ",
+  authDomain: "memorymastery.firebaseapp.com",
+  projectId: "memorymastery",
+  storageBucket: "memorymastery.firebasestorage.app",
+  messagingSenderId: "439635305999",
+  appId: "1:439635305999:web:374ab9a9818b04c3dfe57a",
+  measurementId: "G-HJZ48TJ1DW"
+};
 export const configured=()=>!firebaseConfig.apiKey.startsWith('YOUR_');
 export async function connectFirebase(){if(!configured())return null;const [{initializeApp},{getAuth},{getFirestore}]=await Promise.all([import('https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js'),import('https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js'),import('https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js')]);const app=initializeApp(firebaseConfig);return{app,auth:getAuth(app),db:getFirestore(app)}}
